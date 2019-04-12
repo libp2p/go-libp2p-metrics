51872b6a0c76de8085352081b7db8a21ce80e04e
Raúl Kripalani <raul@protocol.ai>
2019-04-12 19:12:44 +0100

    WIP consolidate go-libp2p skeleton.

---
+++ deprecated.go
@@ -0,0 +1,15 @@
+package metrics
+
+import moved "github.com/libp2p/go-libp2p/metrics"
+
+// Deprecated: use github.com/libp2p/go-libp2p/metrics.Reporter instead.
+type Reporter = moved.Reporter
+
+// Deprecated: use github.com/libp2p/go-libp2p/metrics.Stats instead.
+type Stats = moved.Stats
+
+// Deprecated: use github.com/libp2p/go-libp2p/metrics.BandwidthCounter instead.
+type BandwidthCounter = moved.BandwidthCounter
+
+// Deprecated: use github.com/libp2p/go-libp2p/metrics.NewBandwidthCounter instead.
+var NewBandwidthCounter = moved.NewBandwidthCounter
--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/libp2p/go-libp2p-metrics
 
 require (
 	github.com/libp2p/go-flow-metrics v0.0.1
+	github.com/libp2p/go-libp2p v0.0.11
 	github.com/libp2p/go-libp2p-peer v0.0.1
 	github.com/libp2p/go-libp2p-protocol v0.0.1
 )
+
+replace github.com/libp2p/go-libp2p => ../go-libp2p
