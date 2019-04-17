dbcc7713b2ba5db5c613bdb2fe3c667d23fe6673
Raúl Kripalani <raul@protocol.ai>
2019-04-17 13:48:04 +0100

    alias deprecated funcs by proxying calls.

--- deprecated.go
+++ deprecated.go
@@ -2,14 +2,16 @@ package metrics
 
 import moved "github.com/libp2p/go-libp2p-core/metrics"
 
-// Deprecated: use github.com/libp2p/go-libp2p/metrics.Reporter instead.
+// Deprecated: use github.com/libp2p/go-libp2p-core/metrics.Reporter instead.
 type Reporter = moved.Reporter
 
-// Deprecated: use github.com/libp2p/go-libp2p/metrics.Stats instead.
+// Deprecated: use github.com/libp2p/go-libp2p-core/metrics.Stats instead.
 type Stats = moved.Stats
 
-// Deprecated: use github.com/libp2p/go-libp2p/metrics.BandwidthCounter instead.
+// Deprecated: use github.com/libp2p/go-libp2p-core/metrics.BandwidthCounter instead.
 type BandwidthCounter = moved.BandwidthCounter
 
-// Deprecated: use github.com/libp2p/go-libp2p/metrics.NewBandwidthCounter instead.
-var NewBandwidthCounter = moved.NewBandwidthCounter
+// Deprecated: use github.com/libp2p/go-libp2p-core/metrics.NewBandwidthCounter instead.
+func NewBandwidthCounter() *moved.BandwidthCounter {
+	return moved.NewBandwidthCounter()
+}
