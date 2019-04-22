04911c6a077ef9d646b0d7a1042fca6e1e07ac49
Raúl Kripalani <raul@protocol.ai>
2019-04-22 14:16:09 +0100

    rename moved pkg alias to core; upgrade deps.

--- deprecated.go
+++ deprecated.go
@@ -1,17 +1,17 @@
 package metrics
 
-import moved "github.com/libp2p/go-libp2p-core/metrics"
+import core "github.com/libp2p/go-libp2p-core/metrics"
 
 // Deprecated: use github.com/libp2p/go-libp2p-core/metrics.Reporter instead.
-type Reporter = moved.Reporter
+type Reporter = core.Reporter
 
 // Deprecated: use github.com/libp2p/go-libp2p-core/metrics.Stats instead.
-type Stats = moved.Stats
+type Stats = core.Stats
 
 // Deprecated: use github.com/libp2p/go-libp2p-core/metrics.BandwidthCounter instead.
-type BandwidthCounter = moved.BandwidthCounter
+type BandwidthCounter = core.BandwidthCounter
 
 // Deprecated: use github.com/libp2p/go-libp2p-core/metrics.NewBandwidthCounter instead.
-func NewBandwidthCounter() *moved.BandwidthCounter {
-	return moved.NewBandwidthCounter()
+func NewBandwidthCounter() *core.BandwidthCounter {
+	return core.NewBandwidthCounter()
 }
